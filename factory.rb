10955a86efd04b6d71bed6b08e24338df7a94de8
RichardSyfer <deadwalker87@gmail.com>
2018-05-13 20:11:45 +0300

    finished refactoring

--- factory.rb
+++ factory.rb
@@ -1,20 +1,68 @@
 class Factory
+  @kls_name = {}
+
   def self.new(*attributes, &block)
-    Class.new do
-      attr_accessor *attributes
+    raise ArgumentError, 'wrong number of arguments (given 0, expected 1+)' \
+          if attributes.length < 1
+
+    cls_name = nil
+    cls_name = attributes.shift if attributes.first.is_a?(String)
+    if cls_name
+      raise NameError, 'class name should to be constant' \
+            if cls_name[0] == cls_name.downcase[0]
+    end
+
+    self.class.define_method :inspect do
+    # define_method :inspect do # for ruby-version < 2.5.x
+      if cls_name
+        "Factory::#{cls_name}"
+      else
+        super
+      end
+    end
+
+    cls = Class.new do
+      attr_accessor *attributes # self.send(:attr_accessor, *attributes)
 
       define_method :initialize do |*args|
-        raise 'Error number of arguments' if attributes.count != args.count
-        attributes.zip(args).each do |inst_attr, val|
-          send("#{inst_attr}=", val)
-        end
+        raise ArgumentError,  "wrong number of arguments (given 0," \
+                              "expected #{attributes.count})" \
+                              if attributes.count != args.count
+        attributes.zip(args).each { |inst_attr, val| send("#{inst_attr}=", val) }
       end
+
+      class_eval &block if block_given?
 
       define_method :[] do |arg|
         arg.is_a?(Numeric) ? send(attributes[arg]) : send(arg)
       end
 
-      class_eval &block if block_given?
+      define_method :[]= do |key, val|
+        key.is_a?(Numeric) ? send("#{attributes[key]}=", val) : send("#{key}=", val)
+      end
+
+      define_method :to_a do |*args|
+        arr = []
+        unless args.empty?
+          if args.length == 1
+            arr = send(attributes[*args])
+          else
+            args.each { |arg| arr << send(attributes[arg]) }
+          end
+        else
+          attributes.each { |attr| arr << send(attr) }
+        end
+        arr
+      end
+      alias_method :values, :to_a
+
+      define_method :values_at do |*args|
+        args.empty? ? [] : values(*args)
+      end
+
+      define_method :to_h  do |arg = nil|
+        arg ? send(arg) : attributes.zip(self.values).to_h
+      end
 
       define_method :each do |&block|
         attributes.each { |attr| block.call(send(attr)) }
@@ -29,9 +77,10 @@ class Factory
       end
 
       define_method :to_s do
+        kls_name = "#{ 'Factory::' + cls_name }" if cls_name
         str = ''
         each_pair{ |attr, val| str += " #{attr}=\'#{val}\'" }
-        "<factory #{self.class} #{str}>"
+        "<factory #{kls_name || self.class}#{str}>"
       end
       alias_method :inspect, :to_s
 
@@ -43,6 +92,33 @@ class Factory
         attributes.count
       end
       alias_method :length, :size
+
+      define_method :== do |other|
+        return true if self.class == other.class && \
+                       self.members == other.members && \
+                       self.values == other.values
+        false
+      end
+
+      define_method :eql? do |other|
+        return true if self.to_h == other.to_h
+        false
+      end
+
+      define_method :hash do
+        self.values.hash
+      end
     end
+
+    if cls_name
+      kls = { cls_name.to_sym => cls}
+      @kls_name.update(kls)
+    end
+
+    return cls
+    end
+
+  def Object.const_missing(const)
+    @kls_name[const]
   end
 end
--- spec/factory_spec.rb
+++ spec/factory_spec.rb
@@ -28,9 +28,19 @@ RSpec.describe Factory do
       expect(person).to be_instance_of Person
     end
 
+    it 'should create instance of Factory::MyCls' do
+      Factory.new('MyCls', :name)
+      my_cls = Factory::MyCls.new('Instance Name')
+      expect(my_cls).to be_instance_of Factory::MyCls
+    end
+
     it 'raise error when creating instance with wrong arguments number' do
       expect { Person.new('Jason', 'Timbuktu', 'KT, Street st 209') }
-        .to raise_error RuntimeError, 'Error number of arguments'
+        .to raise_error ArgumentError
+    end
+
+    it 'getting access to instance attr by specified field (method)' do
+      expect(person.name).to eq 'Jane Doe'
     end
 
     it 'getting access to instance attr by [:sym]' do
@@ -48,31 +58,94 @@ RSpec.describe Factory do
   describe 'instance_methods' do
     let(:person) { Person.new('Jane Doe', 'LA, Greenwood Sq 223') }
 
-    it 'each method return instance_vars values' do
+    it ':[]= Sets the value of the given struct member' do
+      person[:name] = 'Katrine Show'
+      expect(person[:name]).to eq 'Katrine Show'
+    end
+
+    it ':[]= Sets the value of the given index' do
+      person[1] = 'New City'
+      expect(person[:address]).to eq 'New City'
+    end
+
+    it ':to_a Returns the values for this struct as an Array.' do
+      expect(person.to_a).to eq ["Jane Doe", "LA, Greenwood Sq 223"]
+    end
+
+    it ':to_a[index] Returns the attribute\'s value by specified index' do
+      expect(person.to_a[0]).to eq 'Jane Doe'
+    end
+
+    it ':to_a[range] Returns the attribute\'s values by specified index range' do
+      expect(person.to_a[0..1]).to eq ["Jane Doe", "LA, Greenwood Sq 223"]
+    end
+
+    it ':values Returns the values for this struct as an Array.' do
+      expect(person.values).to eq ["Jane Doe", "LA, Greenwood Sq 223"]
+    end
+
+    it ':values[index] Returns the attribute\'s value by specified index' do
+      expect(person.values[0]).to eq 'Jane Doe'
+    end
+
+    it ':values_at index Returns the attribute\'s value by specified index' do
+      expect(person.values_at 0).to eq 'Jane Doe'
+    end
+
+    it ':values_at indexes Returns the attribute\'s values by specified indexes' do
+      Person2 = described_class.new(:name, :address, :zip)
+      prs2 = Person2.new("Bill", "City", 123)
+      expect(prs2.values_at 0, 2).to eq ["Bill", 123]
+    end
+
+    it ':to_h Returns the hash of instance attributes' do
+      inst_hash = { name: 'Jane Doe', address: 'LA, Greenwood Sq 223' }
+      expect(person.to_h).to eq inst_hash
+    end
+
+    it ':to_h[:key] Returns the attribute\'s value by specified key' do
+      expect(person.to_h[:name]).to eq 'Jane Doe'
+    end
+
+    it ':each Returns instance_vars values' do
       str = []
       person.each { |x| str << x }
       expect(str).to eq ['Jane Doe', 'LA, Greenwood Sq 223']
     end
 
-    it 'each_pair method return keys and values of instance_vars' do
+    it ':each_pair Returns keys and values of instance_vars' do
       str = []
       person.each_pair { |k, v| str << [k, v] }
       expect(str).to eq [[:name, 'Jane Doe'],
                          [:address, 'LA, Greenwood Sq 223']]
     end
 
-    it 'to_s and inspect methods return instance in string form' do
-      expect(person.to_s).to eq "<factory Person  name='Jane Doe' address='LA, Greenwood Sq 223'>"
-      expect(person.inspect).to eq "<factory Person  name='Jane Doe' address='LA, Greenwood Sq 223'>"
+    it ':to_s and :inspect Return instance in string form' do
+      expect(person.to_s).to eq "<factory Person name='Jane Doe' address='LA, Greenwood Sq 223'>"
+      expect(person.inspect).to eq "<factory Person name='Jane Doe' address='LA, Greenwood Sq 223'>"
     end
 
-    it 'members method return array of instance_vars' do
+    it ':members Returns array of instance_vars' do
       expect(person.members).to eq [:name, :address]
     end
 
-    it 'size and length methods return count of instance_vars' do
+    it ':size and :length Return count of instance_vars' do
       expect(person.size).to eq(2)
       expect(person.length).to eq(2)
+    end
+
+    it ':== Returns true if other has the same struct subclass and has equal member values ' do
+      person_2 = Person.new('Jane Doe', 'LA, Greenwood Sq 223')
+      expect(person == person_2).to eq true
+    end
+
+    it ':eql? Returns true if other has the same struct subclass and has equal member values ' do
+      person_2 = Person.new('Jane Doe', 'LA, Greenwood Sq 223')
+      expect(person.eql?(person_2)).to eq true
+    end
+
+    it ':hash Returns a hash value based on this struct\'s contents' do
+      expect(person.hash.is_a?(Numeric)).to eq true
     end
   end
 end
